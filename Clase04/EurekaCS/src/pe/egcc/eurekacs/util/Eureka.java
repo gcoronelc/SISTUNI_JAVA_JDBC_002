f940bf472d8312cb46d3e0fef372e731fe29320a
Gustavo Coronel <gcoronelc@gmail.com>
2016-03-10 09:25:19 -0500

    Clase 04

---
+++ Clase04/EurekaCS/src/pe/egcc/eurekacs/util/Eureka.java
@@ -0,0 +1,18 @@
+package pe.egcc.eurekacs.util;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public final class Eureka {
+
+  private Eureka() {
+  }
+
+  // Constantes del CRUD
+  public static String CRUD_EDITAR = "EDITAR";
+  public static String CRUD_ELIMINAR = "ELIMINAR";
+  public static String CRUD_NUEVO = "NUEVO";
+  
+  
+}
